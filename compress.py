933e505991273ccc469b6336bb76f13179af2319
minesweeter <joceran.g@gmail.com>
2022-10-13 00:58:28 +0200

    Simulator working but still bugs

---
+++ compress.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import obja
+import numpy as np
+import sys
+import simulator as sim
+
+class Decimater(obja.Model):
+    """
+    A simple class that decimates a 3D model stupidly.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def contract(self, output):
+        """
+        Decimates the model stupidly, and write the resulting obja in output.
+        """
+        operations = []
+        simulator = sim.Simulator(self.vertices, self.faces)
+
+        for (vertex_index, vertex) in enumerate(self.vertices):
+            operations.append(('vertex', (vertex_index, vertex)))
+        for (face_index, face) in enumerate(self.faces):
+            operations.append(('face', (face_index, face)))
+        
+        for _ in range(20):
+            try:
+                deletion = simulator.deletion()
+            except ValueError:
+                print("Error")
+                break
+            # deletion = dict(
+            #     i_del=v_del.idx,
+            #     v_del=v_del.coordinates,
+            #     f_del=tuple(f_del),
+            #     f_modified=f_modified,
+            # )
+            operations.append(('del_face', deletion['f_del'][0]))
+            operations.append(('del_face', deletion['f_del'][1]))
+            for f_modified in deletion['f_modified']:
+                operations.append(('edit_face', (f_modified, deletion['i_del'], deletion['i_split'])))
+
+        # Write the result in output file
+        output_model = obja.Output(output, random_color=True)
+
+        for operation in operations:
+            ty, args = operation
+            if ty == "vertex":
+                (index, value) = args
+                output_model.add_vertex(index, value)
+            elif ty == "face":
+                (index, value) = args
+                output_model.add_face(index, value)   
+            elif ty == "del_face":
+                face_id = args
+                face = self.faces[face_id]
+                face.a = 0
+                face.b = 0
+                face.c = 0
+                output_model.edit_face(face_id, face)
+            elif ty == "edit_face":
+                (face_id, i_del, i_split) = args
+                face = self.faces[face_id]
+                v_idx_in_face = [face.a, face.b, face.c].index(i_del)
+                setattr(face, ['a', 'b', 'c'][v_idx_in_face], i_split)
+                self.faces[face_id] = face
+                output_model.edit_face(face_id, face)
+            else:
+                (index, value) = args
+                output_model.edit_vertex(index, value)
+
+def main():
+    """
+    Runs the program on the model given as parameter.
+    """
+    np.seterr(invalid = 'raise')
+    model = Decimater()
+    model.parse_file('example/suzanne.obj')
+
+    with open('example/suzanne.obja', 'w') as output:
+        model.contract(output)
+
+
+if __name__ == '__main__':
+    main()
--- simulator.py
+++ simulator.py
@@ -1,5 +1,6 @@
+from multiprocessing.sharedctypes import Value
 import numpy as np
-from .. import obja
+import obja
 
 class Vertex:
     """Classe simplifiant la représentation/manipulation de la connectivité d'un sommet
@@ -12,15 +13,15 @@ class Vertex:
             face_idx = [face.a, face.b, face.c]
             if idx in face_idx:
                 for neighbor in face_idx:
-                    if neighbor != idx and neighbor not in self._neighbours:
-                        self._neighbours.append(neighbor)
+                    self.add_neighbor(neighbor)
         self.is_deleted = False
     
     def delete_neighbor(self, v_del_idx):
         del(self.neighbours[self.neighbours.index(v_del_idx)])
     
     def add_neighbor(self, v_add_idx):
-        self.neighbours.append(v_add_idx)
+        if v_add_idx not in self.neighbours and v_add_idx != self.idx:
+            self.neighbours.append(v_add_idx)
 
 
 class Face(obja.Face):
@@ -28,13 +29,32 @@ class Face(obja.Face):
         super().__init__(a, b, c, visible)
         self.is_deleted = False
 
+# >> 266
+# Error : vdel.idx=266
+# 266
+# 268
+# [248, 264, 268]
+# [246, 248, 266, 270, 244]
+# [266, 248, 264]
 
-class Model:
+class Simulator:
     """
     Classe permettant la simulation d'une compression d'un objet 3D suivant l'algorithme SQUEEZE 
     """
-    def __init__(self):
-        None
+    def __init__(self, vertices, faces):
+        self._vertices = [Vertex(idx, vertex, faces) for idx, vertex in enumerate(vertices)]
+        self._faces = [Face(face.a, face.b, face.c) for face in faces]
+        
+        problem = []
+        for vertex in range(len(vertices)):
+            n_faces = 0
+            for face in faces:
+                if vertex in [face.a, face.b, face.c]:
+                    n_faces += 1
+            if n_faces < 3:
+                problem.append(vertex)
+        print(problem)
+        assert not problem
 
     def load_M(self, vertices, faces):
         """Load le model initial.
@@ -61,40 +81,59 @@ class Model:
         """
 
         # choose next oriented edge to collapse
-        found = True
+        found = False
         while not found:
             v_del = self._vertices[np.random.randint(0, len(self._vertices))]
             found = not v_del.is_deleted
         v_del.is_deleted = True
-        
-        # delete the edges linking v_del to its neighbours and link them to v_split
-        neighbours = v_del._neighbours
+        print(v_del.idx)
+        neighbours = v_del.neighbours
         v_split = self._vertices[neighbours[np.random.randint(0, len(neighbours))]]
-        for neighbor_idx in neighbours:
-            neighbor = self._vertices[neighbor_idx]
-            neighbor.delete_neighbor(v_del.idx)
-            neighbor.add_neighbor(v_split.idx)
+
+        # print(v_del.idx)
+        # print(v_split.idx)
+        # print(v_del.neighbours)
+        # print(v_split.neighbours)
+        # for i, face in enumerate(self._faces):
+        #     if not face.is_deleted and v_del.idx in [face.a, face.b, face.c]:
+        #         print([face.a, face.b, face.c])
 
         # delete the two faces that contain v_del and v_split
         f_del = []
-        for face in enumerate(self._faces):
+        for i, face in enumerate(self._faces):
             if not face.is_deleted and v_del.idx in [face.a, face.b, face.c] and v_split.idx in [face.a, face.b, face.c]:
-                f_del.append(face)
-                face.is_deleted = True
-        assert(len(f_del == 2))
+                f_del.append(i)
+                self._faces[i].is_deleted = True
+        if len(f_del) != 2:
+            print(f"Error : v_del.idx={v_del.idx} - v_split.idx={v_split.idx}")
+            print(len(f_del))
+            for i, face in enumerate(self._faces):
+                if v_del.idx in [face.a, face.b, face.c]:
+                    print([face.a, face.b, face.c])
+            raise(ValueError)
         
         # modify the faces that contain v_del only
         f_modified = []
         for i, face in enumerate(self._faces):
             if not face.is_deleted and v_del.idx in [face.a, face.b, face.c]:
                 f_modified.append(i)
-                v_idx_in_face = [face.a, face.b, face.c].index(v_del)
+                v_idx_in_face = [face.a, face.b, face.c].index(v_del.idx)
                 setattr(face, ['a', 'b', 'c'][v_idx_in_face], v_split.idx)
+                self._faces[i] = face
+        
+        # delete the edges linking v_del to its neighbours and link them to v_split
+        for i in neighbours:
+            neighbor = self._vertices[i]
+            neighbor.delete_neighbor(v_del.idx)
+            neighbor.add_neighbor(v_split.idx)
+            v_split.add_neighbor(i)
+            self._vertices[i] = neighbor
+        self._vertices[v_split.idx] = v_split
+        self._vertices[v_del.idx] = v_del
 
-        if 1 == 0:
-            raise ValueError
         result = dict(
             i_del=v_del.idx,
+            i_split=v_split.idx,
             v_del=v_del.coordinates,
             f_del=tuple(f_del),
             f_modified=f_modified,
